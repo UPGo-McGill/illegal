769ffed97561e04aa651d13eb2fce91e727cafa4
amy <amy.bolt@mail.mcgill.ca>
2019-05-16 16:16:07 -0400

    property file

--- R/02 data_import.R
+++ R/02 data_import.R
@@ -6,3 +6,9 @@ source("R/01 helper_functions.R")
 montreal <- read_sf(dsn = ".", layer = "plateau") 
 
 plateau <- filter(plateau, NOM == "Le Plateau-Mont-Royal")
+
+## import airbnb listings from 2018
+
+property_file <- read_csv("Data/Montreal_property.csv")
+
+
