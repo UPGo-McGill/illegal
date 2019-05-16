2a0221cc57918aad0a857b558006bb0356a4bc7c
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-16 16:21:19 -0400

    plateau buffer

--- R/02 data_import.R
+++ R/02 data_import.R
@@ -3,12 +3,24 @@
 source("R/01 helper_functions.R")
 
 ## import plateau dataset
-montreal <- read_sf(dsn = ".", layer = "plateau") 
 
-plateau <- filter(plateau, NOM == "Le Plateau-Mont-Royal")
+montreal <- read_sf(dsn = ".", layer = "plateau") %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+  st_transform(3347)
+View(plateau)
+
+plateau_buff <-
+  montreal %>%
+  filter (CODEID=="5") %>%
+  summarize(geometry = st_union(geometry)) %>% 
+  st_buffer(200)
+
+plot (plateau_buff ["geometry"])
 
 ## import airbnb listings from 2018
 
 property_file <- read_csv("Data/Montreal_property.csv")
+
+
 
 
