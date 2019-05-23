5248ef5030b827c818d68e7d65621839950ec195
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-23 15:10:09 -0400

    osm scrpt

--- R/03- osm.R
+++ R/03- osm.R
@@ -24,21 +24,25 @@ hf_osm <-
   add_osm_feature(key = "highway") %>%
   osmdata_sf()
 
-
+available_features()
 
 jhf_streets <-
   rbind(jhf_osm$osm_polygons %>% st_cast("LINESTRING"), jhf_osm$osm_lines) %>%
   as_tibble() %>%
   st_as_sf() %>%
   st_transform(26918) %>%
-  select(osm_id, name, geometry)jhf_dodgr <-
+  select(osm_id, name, geometry)
+
+jhf_dodgr <-
   dodgr_streetnet("queens new york city") %>%
   weight_streetnet() %>%
   dodgr_to_sf() %>%
   st_sf() %>%
   as_tibble() %>%
   st_as_sf() %>%
-  st_transform(26918)jhf_dodgr <-
+  st_transform(26918)
+
+jhf_dodgr <-
   target_neighbourhoods %>%
   filter(nbhd == "Jackson Heights/Flushing") %>%
   st_buffer(2500) %>%
