e5408b2bd8527bfa83ea9d607168e7b07c47f2a1
David Wachsmuth <david.wachsmuth@mcgill.ca>
2019-05-23 16:36:07 -0400

    OSM code

--- R/03- osm.R
+++ R/03- osm.R
@@ -1,35 +1,18 @@
-
-library(extrafont)
-library(osmdata)
-library(sf)
-library(smoothr)
-library(tidycensus)
-library(tidyverse)
-library(tigris)
-library(tmap)
-library(tmaptools)
-library(units)
-library(dodgr)
-library(stplanr)
-
-
-plateau_osm <-
-  plateau_listings %>%
-  # filter(nbhd == "plateau") %>%
-  st_transform(4326) %>%
-  st_bbox() %>%
-  bb(ext = 1.2) %>%
-  as.vector() %>%
-  opq() %>%
-  add_osm_feature(key = "highway") %>%
+plateau_roads <- 
+  getbb("plateau-mont-royal montreal") %>% 
+  opq() %>% 
+  add_osm_feature(key = "highway") %>% 
   osmdata_sf()
 
-plateau_streets <-
-  rbind(plateau_osm$osm_polygons %>% st_cast("LINESTRING"), plateau_osm$osm_lines) %>%
-  as_tibble() %>%
-  st_as_sf() %>%
-  st_transform(4326) %>%
+plateau_streets <- 
+  rbind(plateau_roads$osm_polygons %>% st_cast("LINESTRING"), plateau_roads$osm_lines) %>% 
+  as_tibble() %>% 
+  st_as_sf() %>% 
+  st_transform(26918) %>%
   select(osm_id, name, geometry)
+
+
+
 
 plot(plateau_streets %>% filter(name == "Rue Saint-Denis"))
 
