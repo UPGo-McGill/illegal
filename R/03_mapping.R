56027f7c7d67e935d60069bc65a669a6eca8f08c
amy <amy.bolt@mail.mcgill.ca>
2019-05-27 12:46:10 -0400

    maps

--- R/03_mapping.R
+++ R/03_mapping.R
@@ -4,7 +4,6 @@ source("R/01_helper_functions.R")
 
 palette <- c('#313695','#fee090','#d73027','#72001a')
 
-
 # All airbnbs within the plateau colour coded by listing type
 tm_shape(st_buffer(plateau, 200)) +
   tm_borders(lwd = 1, alpha = 0.5) + 
@@ -16,4 +15,40 @@ tm_shape(st_buffer(plateau, 200)) +
             frame = FALSE) +
   tm_compass()
 
+# All listings under current legislation colour coded by legality
+tm_shape(st_buffer(plateau, 200)) +
+  tm_borders(lwd = 1, alpha = 0.5) + 
+  tm_shape(plateau) +
+  tm_borders(lwd = 2) +
+  tm_shape(property)+
+  tm_dots(col = "Legal", palette = palette) +
+  tm_layout(legend.position = c("left", "bottom"),
+            frame = FALSE) +
+  tm_compass()
+
+# All legal listings under current legislation
+tm_shape(st_buffer(plateau, 200)) +
+  tm_borders(lwd = 1, alpha = 0.5) + 
+  tm_shape(plateau) +
+  tm_borders(lwd = 2) +
+  tm_shape(filter(property, Legal == TRUE))+
+  tm_dots(col = "green") +
+  tm_layout(legend.position = c("left", "bottom"),
+            frame = FALSE) +
+  tm_compass()
+
 # All illegal listings under current legislation
+tm_shape(st_buffer(plateau, 200)) +
+  tm_borders(lwd = 1, alpha = 0.5) + 
+  tm_shape(plateau) +
+  tm_borders(lwd = 2) +
+  tm_shape(filter(property, Legal == FALSE))+
+  tm_dots(col = "#72001a") +
+  tm_layout(legend.position = c("left", "bottom"),
+            frame = FALSE) +
+  tm_compass()
+
+# analysis - leaving this here until we finalize numbers
+property %>%   
+mean(filter(Legal == FALSE)$FREH)
+  
