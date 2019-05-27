b0f124f74f29c873d30edadd29a8f05f8181260f
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-27 15:53:26 -0400

    census mapper api

--- R/01_helper_functions.R
+++ R/01_helper_functions.R
@@ -11,6 +11,10 @@ library(dodgr)
 library(stplanr)
 library(mapview)
 
+## Census Mapper - Run Personal API
+# options(cancensus.api_key = "CensusMapper_a552c075421feb150db6cbf1d1707230")
+# options(cancensus.cache_path = "~/UPGo/illegal")
+
 
 ## multilistings function
 strr_multilistings <- function(daily, EH = 2, PR = 3, listing_type, host_ID,
