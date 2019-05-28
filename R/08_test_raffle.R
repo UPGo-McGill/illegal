9eef900f0db5a3cf17ae7984154fe8ef47b44820
amy <amy.bolt@mail.mcgill.ca>
2019-05-28 13:28:20 -0400

    raffle

--- R/08_test_raffle.R
+++ R/08_test_raffle.R
@@ -19,5 +19,26 @@ DA_plateau <- DA[lengths(st_intersects(DA, plateau))>0,]
     tm_fill(col="green", alpha = 0.4)+
     tm_shape(st_denis_buff)+
     tm_fill(col="green", alpha = 0.4)
-    
+
+
+DA_plateau$GeoUID <- as.numeric(DA_plateau$GeoUID)
+  
+outliers <- anti_join(st_drop_geometry(property), 
+                      DA_plateau, 
+                      by = c("winner" = "GeoUID")) %>% 
+  left_join(property)    
+
+tm_shape(DA_plateau) +
+  tm_borders() + 
+  tm_shape(outliers$geometry)+
+  tm_dots()
+
+# Percentage of all listings that were removed using the raffle
+nrow(outliers)/nrow(property)
+
+# Percentage of listings within the 200m buffer but not within the plateau boundaries
+  # that were removed using the raffle
+nrow(outliers)/nrow(anti_join(st_drop_geometry(property), 
+                  st_join(property, plateau, join = st_within, left = FALSE), 
+                  by = "Property_ID"))
 
