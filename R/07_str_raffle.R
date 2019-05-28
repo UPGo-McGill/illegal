305b3723752cd6444d9ad754a87ab92fa972b120
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-28 11:03:34 -0400

    raffle script

--- R/07_str_raffle.R
+++ R/07_str_raffle.R
@@ -1,154 +1,14 @@
-#' Raffle to assign STR listings to administrative units for spatial analysis
-#' 
-#' A function for probablistically assigning STR listings to administrative
-#' geographies (e.g. census tracts) based on reported latitude/longitude.
-#' The function works by combining a known probability density function (e.g.
-#' Airbnb's spatial obfuscation of listing locations) with an additional source
-#' of information about possible listing locations--either population or housing
-#' densities.
+############### RAFFLE FUNCTION ##################
 
+## Load helpers
 
-## 1. Load libraries -----------------------------------------------------------
+source("R/01_helper_functions.R")
 
-lapply(c("sf","dplyr","spatstat","polyCub", "purrr"),
-       library, character.only = TRUE)
+###
 
-
-## 2. Point setup function, returns points -------------------------------------
-
-raffle_setup_points <- function(points, point_ID) {
-  
-  points <- 
-    points %>%
-    filter(!! point_ID > 0) %>%
-    arrange(!! point_ID)
-  
-  points
-}
-
-
-## 3. Polygon setup function, returns polys ------------------------------------
-
-raffle_setup_polys <- function(polys, poly_ID, units){
-  
-  polys <- 
-    polys %>%
-    filter(!! units > 0) %>%
-    st_set_agr("constant") %>% # Prevent warnings from the st operations
-    mutate(
-      !! poly_ID := as.character(!! poly_ID), # Make sure poly_ID is not factor
-      poly_area = st_area(.) # Calculate polygon areas
-    ) %>% 
-    st_set_agr("constant")
-  
-  polys
-}
-
-
-## 4. Intersect point buffers with polygons, returns intersects ----------------
-
-raffle_intersect <- function(points, polys, units, distance) {
-  
-  units <- enquo(units)
-  
-  # Get point coordinates for future use
-  points <- 
-    points %>%
-    mutate(
-      point_x = st_coordinates(.)[,1], 
-      point_y = st_coordinates(.)[,2]
-    )
-  
-  # Generate buffers and intersect with polygons
-  intersects <-
-    points %>%
-    st_buffer(dist = distance, nQuadSegs = 10) %>% 
-    st_set_agr("constant") %>%
-    st_intersection(polys)
-  
-  # Estimate int_units
-  intersects <-
-    intersects %>%
-    mutate(int_units = as.numeric(!! units * st_area(geometry) / poly_area)) %>% 
-    st_set_agr("constant")
-  
-  # Transform intersects relative to point coordinates
-  intersects <- 
-    intersects %>% 
-    mutate(geometry = pmap(list(geometry, point_x, point_y), ~{
-      ..1 - c(..2, ..3)
-    }) %>% 
-      st_sfc())
-  
-  intersects
-}
-
-
-## 5. PDF helper function, returns vector of probabilities ---------------------
-
-raffle_pdf <- function(x) {
-  dnorm(sqrt(x[,1]^2 + x[,2]^2), mean = 100, sd = 50, log = FALSE) *
-    (1 / (2 * pi))
-}
-
-## 6. Integrate the PDF over intersect polygons, returns intersects ------------
-
-raffle_integrate <- function(intersects) {
-  
-  intersects <- 
-    intersects %>% 
-    mutate(probability = map2_dbl(geometry, int_units, ~{
-      polyCub.midpoint(as(.x, "Spatial"), raffle_pdf) * .y
-    })
-    )
-  
-  intersects
-}
-
-
-## 7. Determine winners, returns points ----------------------------------------
-
-raffle_choose_winner <- function(
-  points, intersects, point_ID, poly_ID, diagnostic) {
-  
-  point_ID <- enquo(point_ID)
-  poly_ID  <- enquo(poly_ID)
-  
-  results <-
-    intersects %>%
-    st_drop_geometry() %>%
-    group_by(!! point_ID)
-  
-  if (diagnostic == TRUE) {
-    results <-
-      results %>%
-      summarize(
-        winner = as.character(base::sample(!! poly_ID, 1, prob = probability)),
-        candidates = list(matrix(
-          c(!! poly_ID, (probability) / sum(probability)), ncol = 2))
-      )
-  } else {
-    results <-
-      results %>%
-      summarize(
-        winner = as.character(base::sample(!! poly_ID, 1, prob = probability))
-      )
-  }
-  
-  points <- left_join(points, results, by = quo_name(point_ID))
-  
-  points
-}
-
-
-## 8. Main compiler to run the whole process -----------------------------------
-
-str_raffle <- function(
-  points, polys, point_ID, poly_ID, units,
-  distance = 200, diagnostic = FALSE, cores = 1) {
-  
-  lapply(c("sf","dplyr","spatstat","polyCub", "purrr"), 
-         library, character.only = TRUE)
+strr_raffle <- function(
+  points, polys, poly_ID, units, distance = 200, diagnostic = FALSE,
+  cores = 1) {
   
   # Check that cores is an integer > 0
   cores <- floor(cores)
@@ -181,36 +41,117 @@ str_raffle <- function(
   points <- as_tibble(points) %>% st_as_sf()
   polys  <- as_tibble(polys)  %>% st_as_sf()
   
-  if (cores >= 2) library(parallel)
-  if (cores >= 2) library(pbapply)
+  # Transform polys CRS to match points
+  if (st_crs(points) != st_crs(polys)) {
+    polys <- st_transform(polys, st_crs(points))
+  }
   
-  point_ID <- enquo(point_ID)
+  # Quote variables
   poly_ID  <- enquo(poly_ID)
   units    <- enquo(units)
   
-  points <- raffle_setup_points(points, point_ID)
-  polys <- raffle_setup_polys(polys, poly_ID, units)
-  intersects <- raffle_intersect(points, polys, Housing, distance)
+  # Initialize helper fields
+  points <-
+    points %>%
+    mutate(.point_x = st_coordinates(.)[,1],
+           .point_y = st_coordinates(.)[,2],
+           .point_ID = seq_len(nrow(points)))
   
-  # Multi-threaded version
+  # Clean up polys and initialize poly_area field
+  polys <-
+    polys %>%
+    filter(!! units > 0) %>%
+    st_set_agr("constant") %>% # Prevent warnings from the st operations
+    mutate(
+      !! poly_ID := as.character(!! poly_ID), # Make sure poly_ID is not factor
+      poly_area = st_area(.) # Calculate polygon areas
+    ) %>%
+    st_set_agr("constant")
+  
+  # Generate buffers and intersect with polygons
+  intersects <-
+    points %>%
+    st_buffer(dist = distance, nQuadSegs = 10) %>%
+    st_set_agr("constant") %>%
+    st_intersection(polys)
+  
+  # Estimate int_units
+  intersects <-
+    intersects %>%
+    mutate(
+      int_units = as.numeric(!! units * st_area(.) / .data$poly_area)) %>%
+    st_set_agr("constant")
+  
+  # Transform intersects relative to point coordinates
+  intersects <-
+    intersects %>%
+    mutate(geometry = purrr::pmap(
+      list(.data$geometry, .data$.point_x, .data$.point_y), ~{
+        ..1 - c(..2, ..3)
+      }) %>%
+        st_sfc())
+  
+  # Multi-threaded version of integration
   if (cores >= 2) {
     
-    clusters <- splitpb(nrow(intersects), cores, nout = 100)
+    clusters <- pbapply::splitpb(nrow(intersects), cores, nout = 100)
     intersects_list <- lapply(clusters, function(x) intersects[x,])
-    cl <- makeForkCluster(cores)
+    cl <- parallel::makeForkCluster(cores)
     
     intersects <-
       intersects_list %>%
-      pblapply(raffle_integrate, cl = cl) %>%
+      pbapply::pblapply(raffle_integrate, cl = cl) %>%
       do.call(rbind, .)
     
-    # Single-threaded version 
+    # Single-threaded version of integration
   } else {
     intersects <- raffle_integrate(intersects)
   }
   
-  points <-
-    raffle_choose_winner(points, intersects, Property_ID, GEOID, diagnostic)
+  # Initialize results object
+  results <-
+    intersects %>%
+    st_drop_geometry() %>%
+    group_by(.data$.point_ID)
   
+  # Choose winners
+  if (diagnostic == TRUE) {
+    results <-
+      results %>%
+      summarize(
+        winner = as.character(
+          base::sample(!! poly_ID, 1, prob = .data$probability)),
+        candidates = list(matrix(
+          c(!! poly_ID, (.data$probability) / sum(.data$probability)),
+          ncol = 2))
+      )
+  } else {
+    results <-
+      results %>%
+      summarize(
+        winner = as.character(
+          base::sample(!! poly_ID, 1, prob = .data$probability))
+      )
+  }
+  
+  # Join winners to point file
+  points <-
+    left_join(points, results, by = ".point_ID") %>%
+    select(-.data$.point_ID, -.data$.point_x, -.data$.point_y)
   points
+}
+
+
+
+raffle_pdf <- function(x) {
+  dnorm(sqrt(x[,1]^2 + x[,2]^2), mean = 100, sd = 50, log = FALSE) *
+    (1 / (2 * pi))
+}
+
+
+raffle_integrate <- function(intersects) {
+  intersects %>%
+    mutate(probability = purrr::map2_dbl(.data$geometry, .data$int_units, ~{
+      polyCub::polyCub.midpoint(as(.x, "Spatial"), raffle_pdf) * .y
+    }))
 }
