c4a6310894c8dfe7afe5a9f7bfd1dc5e91ce9688
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-28 13:56:28 -0400

    sample_subset

--- R/08_test_raffle.R
+++ R/08_test_raffle.R
@@ -42,3 +42,17 @@ nrow(outliers)/nrow(anti_join(st_drop_geometry(property),
                   st_join(property, plateau, join = st_within, left = FALSE), 
                   by = "Property_ID"))
 
+
+
+## Random Subset
+
+
+sample (1:1369, 20)
+sample (1:1752, 20)
+
+sample_st_l <- st_laurent_prop[c(254,388,1323,453,830,798,1208,1324,51,1129,928,981,1270,1096,344,117,141,150,1097,1100),]
+
+sampl_st_d <- st_denis_prop[c(704,825,776,525,410,1084,1134,1046,515,747,1123,905,171,1008,1056,522,852,874,179,901),]          
+
+  
+  
