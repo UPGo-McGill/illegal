dadeadc596ac2ab3482820c67870c346ac63aab6
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-29 10:41:43 -0400

    sample listings

--- R/sample streets.R
+++ R/sample streets.R
@@ -5,7 +5,10 @@ source("R/04_osm.R")
 
 ## Sample from St-D and St-L
 
-sample_st_l <- st_laurent_prop[c(sample(1:1822,50)),]
-sample_st_d <- st_denis_prop[c(sample(1:1307,50)),]
+sample_st_l <- st_laurent_prop %>%
+  filter(Legal ==FALSE)
+sample_st_l <- sample_st_l[c(sample(1:787,50)),]
 
-View(sample_st_d)
+sample_st_d <- st_denis_prop %>%
+  filter(Legal==FALSE)
+sample_st_d <- sample_st_d[c(sample(1:412,50)),]
