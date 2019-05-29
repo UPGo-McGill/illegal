9061eb254cc350c9f430af7a180f3f660faecc0d
d-girard <danielle.girard@mail.mcgill.ca>
2019-05-29 16:49:42 -0400

    update

--- R/05 permit address.R
+++ R/05 permit address.R
@@ -27,5 +27,21 @@ plateau_address %>%
   filter(str_detect(Address, "Saint-Laurent"))
 
 plateau_address %>%
-  filter(str_detect(Address, "Saint-Denis"))%>%
+  filter(str_detect(Address, "Saint-Denis"))
 
+## Map with locations of property with permits (red = airbnb aprox locations
+## blue = permit data actual locations)
+
+  tm_shape(st_buffer(plateau, 200)) +
+    tm_borders(lwd = 1) + 
+    tm_shape(plateau_streets)+
+    tm_lines(col="grey", alpha = 0.5)+
+    tm_shape(candidate_streets)+
+    tm_lines(col = "grey", alpha = 0.5) +
+    tm_shape(plateau) +
+    tm_borders(lwd = 2) +
+    tm_shape(filter(property, Permit==TRUE))+
+    tm_dots(size = 0.05, col="red")+
+    tm_shape(plateau_address)+
+    tm_dots(col = "#72001a") +
+    tm_compass()
--- R/sample streets.R
+++ R/sample streets.R
@@ -7,9 +7,9 @@ source("R/04_osm.R")
 
 sample_st_l <- st_laurent_prop %>%
   filter(Legal ==FALSE)
-sample_st_l <- sample_st_l[c(sample(1:787,50)),]
+sample_st_l <- sample_st_l[c(sample(1:802,50)),]
 
 sample_st_d <- st_denis_prop %>%
   filter(Legal==FALSE)
-sample_st_d <- sample_st_d[c(sample(1:412,50)),]
+sample_st_d <- sample_st_d[c(sample(1:409,50)),]
 View(sample_st_d)
